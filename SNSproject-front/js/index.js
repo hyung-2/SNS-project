637180ad0f574753a384e6d16c9c79e011877287
hyung-2 <nh_0221@naver.com>
2023-08-16 15:33:00 +0900

    회원가입시 서버연동, 꾸미기완료

--- SNSproject-front/js/index.js
+++ SNSproject-front/js/index.js
@@ -2,25 +2,60 @@ const loginBtn = document.querySelector('.login-btn')
 const form = document.querySelector('.form')
 const Logininputs = form.querySelectorAll('input')
 const registerBtn = document.querySelector('.register')
+const blurbox = document.querySelector('.box')
 const registerCon = document.querySelector('.register-container')
 const registerInputs = registerCon.querySelectorAll('input')
-const registerOkBtn = registerCon.querySelector('button')
-const blurbox = document.querySelector('.box')
 const subBtn = document.querySelector('.submit-btn')
+const birth = document.querySelector('.birth')
+const yearOfBirth = document.getElementById('year')
+const monthOfBirth = document.getElementById('month')
+const dayOfBirth = document.getElementById('day')
+
+//로그인 성공시
+loginBtn.addEventListener('click', function(e){
+  window.location.href = '../html/main.html'
+})
 
 
-function clickLoginBtn(e){
-    window.location.href = '../html/main.html'
-}
-
-loginBtn.addEventListener('click', clickLoginBtn)
-
-
+//계정 생성하기 버튼 클릭시(로그인화면에서)
 registerBtn.addEventListener('click', function(){
   registerCon.classList.remove('close')
   blurbox.classList.remove('close')
 })
 
-console.log(registerInputs.values)
+console.log(registerCon.children)
+console.log(registerCon.children[1].id)
 
+// 계정생성창 label꾸미기
+registerCon.addEventListener('keyup',function(e){
+  if(e.target.value !== ''){
+    e.target.previousElementSibling.classList.remove('forcusing')
+  }else{
+    e.target.previousElementSibling.classList.add('forcusing')
+  }
+})
 
+//계정 생성-서버연결
+subBtn.addEventListener('click', function(e){
+  e.preventDefault()
+  // if()
+    fetch('http://127.0.0.1:5002/api/users/register',{
+      method: 'POST', 
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        userId: registerCon.userId.value,
+        name: registerCon.username.value,
+        email: registerCon.useremail.value,
+        birth: `${yearOfBirth.options[yearOfBirth.selectedIndex].value}년 ${monthOfBirth.options[monthOfBirth.selectedIndex].value}월 ${dayOfBirth.options[dayOfBirth.selectedIndex].value}일`,
+        password: registerCon.userPw.value,
+        repassword: registerCon.userPw2.value,
+      })
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(e => console.log(e))
+
+      registerCon.classList.add('close')
+      blurbox.classList.add('close')
+  
+})
